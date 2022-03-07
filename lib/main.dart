4671dcc8bef5b8b0d63546d680b2d95820398084
Aidar Maratbekov <maratbekovaidar@gmail.com>
2022-03-08 05:26:43 +0600

    feat(wave): release sound wave and intensity wave

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,382 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:async';
+import 'dart:math';
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:mic_stream/mic_stream.dart';
+
+enum Command {
+  start,
+  stop,
+  change,
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+const AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+void main() => runApp(const MicStreamExampleApp());
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MicStreamExampleApp extends StatefulWidget {
+  const MicStreamExampleApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MicStreamExampleAppState createState() => _MicStreamExampleAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MicStreamExampleAppState extends State<MicStreamExampleApp>
+    with SingleTickerProviderStateMixin, WidgetsBindingObserver {
+  Stream? stream;
+  late StreamSubscription listener;
+  List<int>? currentSamples = [];
+  List<int> visibleSamples = [];
+  int? localMax;
+  int? localMin;
 
-  void _incrementCounter() {
+
+  // Refreshes the Widget for every possible tick to force a rebuild of the sound wave
+  late AnimationController controller;
+
+  final Color _iconColor = Colors.white;
+  bool isRecording = false;
+  bool memRecordingState = false;
+  late bool isActive;
+  DateTime? startTime;
+
+  int page = 0;
+  List state = ["SoundWavePage", "IntensityWavePage", "InformationPage"];
+
+
+  @override
+  void initState() {
+    debugPrint("Init application");
+    super.initState();
+    WidgetsBinding.instance!.addObserver(this);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      initPlatformState();
     });
   }
 
+  void _controlPage(int index) => setState(() => page = index);
+
+  // Responsible for switching between recording / idle state
+  void _controlMicStream({Command command = Command.change}) async {
+    switch (command) {
+      case Command.change:
+        _changeListening();
+        break;
+      case Command.start:
+        _startListening();
+        break;
+      case Command.stop:
+        _stopListening();
+        break;
+    }
+  }
+
+  Future<bool> _changeListening() async =>
+      !isRecording ? await _startListening() : _stopListening();
+
+
+  late int bytesPerSample;
+  late int samplesPerSecond;
+
+  Future<bool> _startListening() async {
+    debugPrint("START LISTENING");
+    if (isRecording) return false;
+    // if this is the first time invoking the microphone()
+    // method to get the stream, we don't yet have access
+    // to the sampleRate and bitDepth properties
+    debugPrint("wait for stream");
+    stream = await MicStream.microphone(
+        audioSource: AudioSource.DEFAULT,
+        sampleRate: 16000,
+        channelConfig: ChannelConfig.CHANNEL_IN_MONO,
+        audioFormat: AUDIO_FORMAT);
+    // after invoking the method for the first time, though, these will be available;
+    // It is not necessary to setup a listener first, the stream only needs to be returned first
+    debugPrint("Start Listening to the microphone, sample rate is ${await MicStream.sampleRate}, bit depth is ${await MicStream.bitDepth}, bufferSize: ${await MicStream.bufferSize}");
+    bytesPerSample = (await MicStream.bitDepth)! ~/ 8;
+    samplesPerSecond = (await MicStream.sampleRate)!.toInt();
+    localMax = null;
+    localMin = null;
+
+    setState(() {
+      isRecording = true;
+      startTime = DateTime.now();
+    });
+    visibleSamples = [];
+    listener = stream!.listen(_calculateSamples);
+    return true;
+  }
+
+  void _calculateSamples(samples) {
+    if (page == 0) {
+      _calculateWaveSamples(samples);
+    } else if (page == 1) {
+      _calculateIntensitySamples(samples);
+    }
+  }
+
+  void _calculateWaveSamples(samples) {
+    bool first = true;
+    visibleSamples = [];
+    int tmp = 0;
+    for (int sample in samples) {
+      if (sample > 128) sample -= 255;
+      if (first) {
+        tmp = sample * 128;
+      } else {
+        tmp += sample;
+        visibleSamples.add(tmp);
+
+        localMax ??= visibleSamples.last;
+        localMin ??= visibleSamples.last;
+        localMax = max(localMax!, visibleSamples.last);
+        localMin = min(localMin!, visibleSamples.last);
+
+        tmp = 0;
+      }
+      first = !first;
+    }
+    debugPrint(visibleSamples.toString());
+  }
+
+  void _calculateIntensitySamples(samples) {
+    currentSamples ??= [];
+    int currentSample = 0;
+    eachWithIndex(samples, (i, int sample) {
+      currentSample += sample;
+      if ((i % bytesPerSample) == bytesPerSample-1) {
+        currentSamples!.add(currentSample);
+        currentSample = 0;
+      }
+    });
+
+    if (currentSamples!.length >= samplesPerSecond/10) {
+      visibleSamples.add(currentSamples!.map((i) => i).toList().reduce((a, b) => a+b));
+      localMax ??= visibleSamples.last;
+      localMin ??= visibleSamples.last;
+      localMax = max(localMax!, visibleSamples.last);
+      localMin = min(localMin!, visibleSamples.last);
+      currentSamples = [];
+      setState(() {});
+    }
+  }
+
+  bool _stopListening() {
+    if (!isRecording) return false;
+    print("Stop Listening to the microphone");
+    listener.cancel();
+
+    setState(() {
+      isRecording = false;
+      currentSamples = null;
+      startTime = null;
+    });
+    return true;
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    if (!mounted) return;
+    isActive = true;
+
+    Statistics(false);
+
+    controller =
+    AnimationController(duration: const Duration(seconds: 1), vsync: this)
+      ..addListener(() {
+        if (isRecording) setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      })
+      ..forward();
+  }
+
+  Color _getBgColor() => (isRecording) ? Colors.red : Colors.cyan;
+  Icon _getIcon() =>
+      (isRecording) ? const Icon(Icons.stop) : const Icon(Icons.keyboard_voice);
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Plugin: mic_stream :: Debug'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _controlMicStream,
+            child: _getIcon(),
+            foregroundColor: _iconColor,
+            backgroundColor: _getBgColor(),
+            tooltip: (isRecording) ? "Stop recording" : "Start recording",
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.broken_image),
+                label: "Sound Wave",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.broken_image),
+                label: "Intensity Wave",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.view_list),
+                label: "Statistics",
+              )
+            ],
+            backgroundColor: Colors.black26,
+            elevation: 20,
+            currentIndex: page,
+            onTap: _controlPage,
+          ),
+          body: (page == 0 || page == 1)
+              ? CustomPaint(
+            painter: WavePainter(
+              samples: visibleSamples,
+              color: _getBgColor(),
+              localMax: localMax,
+              localMin: localMin,
+              context: context,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )
+              : Statistics(
+            isRecording,
+            startTime: startTime,
+          )),
     );
   }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      isActive = true;
+      print("Resume app");
+
+      _controlMicStream(
+          command: memRecordingState ? Command.start : Command.stop);
+    } else if (isActive) {
+      memRecordingState = isRecording;
+      _controlMicStream(command: Command.stop);
+
+      print("Pause app");
+      isActive = false;
+    }
+  }
+
+  @override
+  void dispose() {
+    listener.cancel();
+    controller.dispose();
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+}
+
+class WavePainter extends CustomPainter {
+  int? localMax;
+  int? localMin;
+  List<int>? samples;
+  late List<Offset> points;
+  Color? color;
+  BuildContext? context;
+  Size? size;
+
+  // Set max val possible in stream, depending on the config
+  // int absMax = 255*4; //(AUDIO_FORMAT == AudioFormat.ENCODING_PCM_8BIT) ? 127 : 32767;
+  // int absMin; //(AUDIO_FORMAT == AudioFormat.ENCODING_PCM_8BIT) ? 127 : 32767;
+
+  WavePainter({this.samples, this.color, this.context, this.localMax, this.localMin});
+
+  @override
+  void paint(Canvas canvas, Size? size) {
+    this.size = context!.size;
+    size = this.size;
+
+    Paint paint = Paint()
+      ..color = color!
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.stroke;
+
+    if (samples!.isEmpty) {
+      return;
+    }
+
+
+    points = toPoints(samples);
+
+    Path path = Path();
+    path.addPolygon(points, false);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldPainting) => true;
+
+  // Maps a list of ints and their indices to a list of points on a cartesian grid
+  List<Offset> toPoints(List<int>? samples) {
+    List<Offset> points = [];
+    samples ??= List<int>.filled(size!.width.toInt(), (0.5).toInt());
+    double pixelsPerSample = size!.width/samples.length;
+    for (int i = 0; i < samples.length; i++) {
+      var point = Offset(i * pixelsPerSample, 0.5 * size!.height * pow((samples[i] - localMin!)/(localMax! - localMin!), 5));
+      points.add(point);
+    }
+    return points;
+  }
+
+  double project(int val, int max, double height) {
+    double waveHeight = (max == 0) ? val.toDouble() : (val / max) * 0.5 * height;
+    return waveHeight + 0.5 * height;
+  }
+}
+
+class Statistics extends StatelessWidget {
+  final bool isRecording;
+  final DateTime? startTime;
+
+  final String url = "https://github.com/anarchuser/mic_stream";
+
+  const Statistics(this.isRecording, {Key? key, this.startTime}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: <Widget>[
+      const ListTile(
+          leading: Icon(Icons.title),
+          title: Text("Microphone Streaming Example App")),
+      ListTile(
+        leading: const Icon(Icons.keyboard_voice),
+        title: Text((isRecording ? "Recording" : "Not recording")),
+      ),
+      ListTile(
+          leading: const Icon(Icons.access_time),
+          title: Text((isRecording
+              ? DateTime.now().difference(startTime!).toString()
+              : "Not recording"))),
+    ]);
+  }
+}
+
+
+Iterable<T> eachWithIndex<E, T>(
+    Iterable<T> items, E Function(int index, T item) f) {
+  var index = 0;
+
+  for (final item in items) {
+    f(index, item);
+    index = index + 1;
+  }
+
+  return items;
 }
